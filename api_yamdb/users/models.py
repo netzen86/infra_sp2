6811fb8db2fa47414632954604cffe98f7076e63
netzen86 <netzen0su@gmail.com>
2022-09-02 23:17:32 +0500

    исправил pep ошибки

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -64,14 +64,6 @@ class CustomUser(AbstractUser):
         verbose_name_plural = 'Пользователи'
 
     @property
-    def is_admin(self):
-        return self.role == 'admin'
-
-    @property
-    def is_moderator(self):
-        return self.role == 'moderator'
-
-    @property
     def is_user(self):
         return self.role == 'user'
 
